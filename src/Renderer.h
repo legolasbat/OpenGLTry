1f87bb75d65596fc263d1b571b7f95a2b8723513
Noah Hdez Morales <legolasbat@gmail.com>
2021-06-25 09:55:44 +0100

    Renderer Class

--- src/Renderer.h
+++ src/Renderer.h
@@ -2,6 +2,10 @@
 
 #include <GL/glew.h>
 
+#include "VertextArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 #define ASSERT(x) if(!(x)) __debugbreak();
 #ifdef DEBUG
 #define GLCall(x) GLClearError();x;ASSERT(GLLogCall(#x, __FILE__, __LINE__))
@@ -11,3 +15,10 @@
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
+
+class Renderer
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
